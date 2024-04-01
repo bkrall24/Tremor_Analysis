d34d8d885d162826a4a68cbf7e11d8d25a6a3445
bkrall24 <becca.krall@gmail.com>
2024-04-01 16:35:20 -0400

    streamlit improvments

--- tremor_analysis.py
+++ tremor_analysis.py
@@ -33,7 +33,7 @@ def compile_excel_sheets(excel_tables, animals, animal_key, parameters):
     all_freq = []
     for an in animals:
         if int(an) in animal_key.index:
-            st.write('Calcuating animal :'+str(an))
+            st.write_stream('Calculating animal :'+str(an))
             group = animal_key.loc[int(an), 'GROUP']
             column_index = (group, an)
 
@@ -59,19 +59,21 @@ if (excel_file is not None) and (key_file is not None):
     # num_bands = st.number_input(label = 'Enter number of frequency bands', min_value = 2, value = 3)
     # for a in range(num_bands):
     lower = st.number_input(label = 'Enter lower bound of tremor band', min_value = 2, max_value = 25, value = 8)
-    higher = st.number_input(label = 'Enter higher bound of tremore band', min_value = 2, max_value = 25, value = 14)
+    higher = st.number_input(label = 'Enter upper bound of tremor band', min_value = 2, max_value = 25, value = 14)
 
     hz = [2,lower, higher, 25]
 
     bin_mins = st.number_input(label = 'Enter bin size in minutes', value = 5)
     exp_mins = st.number_input(label = 'Enter the length of experiment in minutes', value = 60)
 
-    entered = st.button('Enter')
+    entered = st.button('Calculate')
 
     if entered:
         parameters = {'hz': hz, 'bin_time': bin_mins, 'col_time':10.24, 'exp_length' : exp_mins, 'tremor_freq': 1}
 
         excel_tables = pd.read_excel(excel_file, sheet_name = None,  engine='openpyxl')
+        st.text("Excel File Read")
+
         animal_key = pd.read_excel(key_file, index_col = 0,  engine='openpyxl')
 
         animals = [a for a in excel_tables.keys() if a != 'Summary']

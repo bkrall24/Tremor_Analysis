c7092dab54814645d0f042ba4b8f2f90baad69df
Becca <39782419+bkrall24@users.noreply.github.com>
2024-11-07 10:27:02 -0500

    Plotting groups

--- tremor_analysis.py
+++ tremor_analysis.py
@@ -1,8 +1,46 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
 import io
 import re
+
+def get_group_responses(excel_tables, animal_key, group = 2):
+    target_group = animal_key.index[animal_key['GROUP'] == group].tolist()
+
+    for ind, an in enumerate(target_group):
+        sheet = excel_tables[str(an)]
+        data = sheet.iloc[0:256,:].set_index(sheet.columns[0]).iloc[:,2:]
+        new_cols = np.linspace(0, parameters['col_time'] * (sheet.shape[1]-1), sheet.shape[1])/60
+        data.columns = new_cols[2:-1]
+
+        col_bins = np.linspace(0, parameters['exp_length'] , int(parameters['exp_length'] / parameters['bin_time']) +1)
+        time_bins = pd.cut(data.columns, bins = col_bins) 
+        if ind == 0:
+            sum_df = data.T.groupby([time_bins], observed = False).mean().T
+        else:
+            sum_df += data.T.groupby([time_bins], observed = False).mean().T
+
+    average_df = sum_df / len(target_group)
+
+    return average_df
+
+def plot_group_responses(average_df):
+    cmap = plt.get_cmap('turbo')
+    colors = [cmap(i) for i in np.linspace(0, 1, 13)]  # Sample 10 colors
+
+    plt.rcParams['axes.prop_cycle'] = plt.cycler(color=colors)
+    fig, ax = plt.subplots()
+    ax.plot(average_df.index, average_df)
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(1))
+    ax.grid(axis='x', which = 'minor', linestyle=':', color='gray')
+    ax.grid(axis='x', which = 'major', linestyle=':', color='black')
+
+    plt.legend(average_df.columns)
+    st.pyplot(fig) 
+
+    return fig
+
 
 def process_excel_sheet(sheet, parameters):
     
@@ -51,48 +89,92 @@ def compile_excel_sheets(excel_tables, animals, animal_key, parameters):
 
     return freqs, tprs
 
+st.title('Tremor Data Analysis')
 
-st.title('Tremor and Movement Data Analysis')
-excel_file = st.file_uploader(label = "Select Excel File for Tremor Analysis", accept_multiple_files= False)
-key_file = st.file_uploader(label = "Select Excel file with Animal/Group Key", accept_multiple_files = False)
+# File uploader for Excel data and key
+excel_file = st.file_uploader(label="Select Excel File for Tremor Analysis", accept_multiple_files=False)
+key_file = st.file_uploader(label="Select Excel file with Animal/Group Key", accept_multiple_files=False)
 
-if (excel_file is not None) and (key_file is not None):
-    # num_bands = st.number_input(label = 'Enter number of frequency bands', min_value = 2, value = 3)
-    # for a in range(num_bands):
-    lower = st.number_input(label = 'Enter lower bound of tremor band', min_value = 2, max_value = 25, value = 8)
-    higher = st.number_input(label = 'Enter upper bound of tremor band', min_value = 2, max_value = 25, value = 14)
+@st.cache_data
+def load_excel_data(file):
+    return pd.read_excel(file, sheet_name=None)
 
-    hz = [2,lower, higher, 25]
+@st.cache_data
+def load_animal_key(file):
+    return pd.read_excel(file, index_col=0)
 
-    bin_mins = st.number_input(label = 'Enter bin size in minutes', value = 5)
-    exp_mins = st.number_input(label = 'Enter the length of experiment in minutes', value = 60)
+# Check for file uploads
+if excel_file and key_file:
+    # Load data from files with caching
+    excel_tables = load_excel_data(excel_file)
+    animal_key = load_animal_key(key_file)
+    st.text("Excel Files Loaded")
 
-    entered = st.button('Calculate')
+    # Set up user inputs for parameters
+    
 
-    if entered:
-        parameters = {'hz': hz, 'bin_time': bin_mins, 'col_time':10.24, 'exp_length' : exp_mins, 'tremor_freq': 1}
+    bin_mins = st.number_input(label='Enter bin size in minutes', value=5)
+    exp_mins = st.number_input(label='Enter the length of experiment in minutes', value=60)
 
-        excel_tables = pd.read_excel(excel_file, sheet_name = None)
-        st.text("Excel File Read")
 
-        animal_key = pd.read_excel(key_file, index_col = 0)
+    parameters = {
+        'bin_time': bin_mins,
+        'col_time': 10.24,
+        'exp_length': exp_mins,
+        'tremor_freq': 1,
+    }
 
-        # animals = [a for a in excel_tables.keys() if a != 'Summary']
-        animals = list(excel_tables.keys())[1:]
+    group_choice = st.number_input(label = 'Choose group to determine frequency bands', value = 2)
+    # Plot button and group responses
+    if st.button('Plot Frequency Responses'):
+        if group_choice in animal_key['GROUP'].unique():
+            # Generate average_df only if the group is found and store it in session state
+            st.session_state['average_df'] = get_group_responses(excel_tables, animal_key, group=group_choice)
+        else:
+            st.text(f'Group {group_choice} not found in Animal/Group Key')
 
-        freqs, tprs = compile_excel_sheets(excel_tables, animals, animal_key, parameters)
-        freqs = freqs.sort_index(axis = 1)
-        tprs = tprs.sort_index(axis = 1)
+    # Display plot if it exists in session state
+    if 'average_df' in st.session_state:
+        fig = plot_group_responses(st.session_state['average_df'])
+        buffer = io.BytesIO()
+        fig.savefig(buffer, format='png')
+        buffer.seek(0)  # Move to the start of the buffer
 
+        # Download button for the plot
+        st.download_button(
+            label="Download Plot as PNG",
+            data=buffer,
+            file_name="plot.png",
+            mime="image/png"
+        )
+
+    lower = st.number_input(label='Enter lower bound of tremor band', min_value=2, max_value=25, value=8)
+    higher = st.number_input(label='Enter upper bound of tremor band', min_value=2, max_value=25, value=14)
+    parameters['hz'] =  [2, lower, higher, 25]
+
+    # Calculate button and frequency data
+    if st.button('Calculate'):
+        animals = list(excel_tables.keys())[1:]  # Exclude 'Summary' sheet
+
+        # Cache-intensive calculations based on inputs and file content
+        @st.cache_data
+        def compute_frequencies_and_tremor_ratios(excel_tables, animals, animal_key, parameters):
+            freqs, tprs = compile_excel_sheets(excel_tables, animals, animal_key, parameters)
+            return freqs.sort_index(axis=1), tprs.sort_index(axis=1)
+
+        freqs, tprs = compute_frequencies_and_tremor_ratios(excel_tables, animals, animal_key, parameters)
+
+        # Save to Excel and allow download
         buffer = io.BytesIO()
         with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
-            freqs.to_excel(writer, sheet_name = "All Frequencies")
-            tprs.to_excel(writer, sheet_name = 'Tremor Power Ratios')
-        
+            freqs.to_excel(writer, sheet_name="All Frequencies")
+            tprs.to_excel(writer, sheet_name='Tremor Power Ratios')
+            if 'average_df' in st.session_state:
+                st.session_state['average_df'].to_excel(writer, sheet_name = 'Average Responses Group '+str(group_choice))
 
         st.download_button(
             label="Download Excel workbook",
-            data= buffer,
+            data=buffer,
             file_name="workbook.xlsx",
             mime="application/vnd.ms-excel"
         )
